49de7636e125c19aa1a0a9849937556d60ddfa6a
Your Name <you@example.com>
2015-11-09 13:40:13 +0100

    finished the delaying and the encryption

--- axisSocketServer.c
+++ axisSocketServer.c
@@ -19,7 +19,27 @@
 struct ImageThreadParam {
    media_stream *stream;
    int connfd;
+   char* encKey;
+   int maxDelay;
 }; 
+
+char * generateRandomString(int length) 
+{
+	const char *alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRTSUVWXYZ0123456789";
+	int alphabetLength = strlen(alphabet);
+	char* stringbuffer = malloc((length + 1) * sizeof(char));
+	
+	int i;
+	for(i = 0; i < length; ++i)
+	{
+		int random = rand() % alphabetLength;
+		char ch = alphabet[random];
+		stringbuffer[i] = ch;
+	}
+	stringbuffer[length] = '\0';
+	syslog(LOG_INFO, "KEY %s", stringbuffer);
+	return stringbuffer;	
+}
 
 void encryptFrame(unsigned char rowData[], size_t size, const char *keyword) 
 {
@@ -39,7 +59,9 @@ void sendImageFromStream(void *paramPtr)
 	
 	struct ImageThreadParam *param = (struct ImageThreadParam *) paramPtr;
 	media_stream *stream = param->stream;
-	int connfd = param->connfd; 	
+	int connfd = param->connfd; 
+	char *keyword = param->encKey;	
+	int maxDelay = param->maxDelay;
 	media_frame *frame ;
 	void *data;
 
@@ -49,6 +71,12 @@ void sendImageFromStream(void *paramPtr)
 	size_t size = capture_frame_size(frame); 
 	syslog(LOG_INFO, "Image captured");
 	
+	//random delay
+	int randDelay = rand()%maxDelay;
+	syslog(LOG_INFO, "RANDOM DELAY IS %d", randDelay);
+	sleep(randDelay);
+	
+
 	int total_size = size;
 	syslog(LOG_INFO, "Total size %d", total_size);
 	total_size = htonl(total_size);
@@ -58,7 +86,7 @@ void sendImageFromStream(void *paramPtr)
 	It turns the numbers the endian-ness of the numbers from the host format to the network format
 	*/
 	write(connfd, &total_size, sizeof(total_size));
-	syslog(LOG_INFO, "Header sent");
+	syslog(LOG_INFO, "Total size after write() = %d", total_size);
 
 //	Then send the data of the image
 	int row = 0;
@@ -69,7 +97,7 @@ void sendImageFromStream(void *paramPtr)
 		
 	}
 //	encrypt the rowData
-	char *keyword = "sinyata lavina pak leti napred!";
+	//char *keyword = "sinyata lavina pak leti napred!";
 	encryptFrame(rowData, size, keyword);
 
 	write(connfd, rowData, sizeof(rowData));
@@ -105,10 +133,13 @@ uint32_t readInt(int connfd)
 
 int main(int argc, char *argv[])
 {
+	time_t t;
+	srand((unsigned) time(&t));
+
 	openlog(APP_NAME, LOG_PID, LOG_LOCAL4);
 	int listenfd = 0, connfd = 0;
 	struct sockaddr_in serv_addr;
-
+	
 	char sendBuff[1025];
 
 //	Create the socket
@@ -134,6 +165,15 @@ int main(int argc, char *argv[])
  */
 		connfd = accept(listenfd, (struct sockaddr*) NULL, NULL);
 		syslog(LOG_INFO, "Client accepted");
+		// generate key and send it to the client
+		char* encKey = generateRandomString(25);
+		int keyLen = strlen(encKey);
+		syslog(LOG_INFO, "keylen %d", keyLen);
+		syslog(LOG_INFO, "key %s", encKey);
+		keyLen = htonl(keyLen);
+		write(connfd, &keyLen, sizeof(int));
+		write(connfd, encKey, strlen(encKey)*sizeof(char));
+		syslog(LOG_INFO, "Encryption key sent");
 
 		/*
 		Read the four parameters: timeout, width, height and number of images
@@ -143,6 +183,7 @@ int main(int argc, char *argv[])
 		uint32_t width = readInt(connfd);
 		uint32_t height = readInt(connfd);
 		uint32_t numberOfImages = readInt(connfd);
+		uint32_t maxDelay = readInt(connfd);
 
 		syslog(LOG_INFO, "Fixed parameters received: %d, %d, %d, %d", timeout, width, height, numberOfImages);		
 		char streamParamsBuffer[100];
@@ -150,6 +191,8 @@ int main(int argc, char *argv[])
 //		Build the stream parameters with the given width and height
 		sprintf(streamParamsBuffer, "resolution=%dx%d&fps=15",  width, height);
 		syslog(LOG_INFO, "%s", streamParamsBuffer);
+
+
 		media_stream *stream;
 //		Open the stream
 		stream = capture_open_stream(IMAGE_JPEG, streamParamsBuffer);
@@ -160,6 +203,8 @@ int main(int argc, char *argv[])
 			struct ImageThreadParam param;
 			param.stream = stream;
 			param.connfd = connfd;
+			param.encKey = encKey;
+			param.maxDelay = maxDelay;
 			//every time open new thread - synchronization
 			GThread * threadResult;
 			threadResult = g_thread_new( "lala", sendImageFromStream, &param);
@@ -173,7 +218,8 @@ int main(int argc, char *argv[])
 			//sendImageFromStream(param);
 			sleep(timeout);
 		}
-
+		//release the memory for malloc in random string generator
+		free(encKey);
 		syslog(LOG_INFO, "Image sent");
 		close(connfd);
 		exit(EXIT_SUCCESS);
